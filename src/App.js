c4ab6601e962895419e5b0d95c708ad9d4814218
Cayla Ramirez <caylaxnicole@gmail.com>
2018-06-19 14:19:51 -0700

    List Chat Rooms checkpoint: completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RoomList from './components/RoomList';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyDtZYXIUSKrBEjSeSDiRiDO0xYebg6ZUyA",
+    authDomain: "bloc-chat-react-e42d7.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-e42d7.firebaseio.com",
+    projectId: "bloc-chat-react-e42d7",
+    storageBucket: "bloc-chat-react-e42d7.appspot.com",
+    messagingSenderId: "581635321105"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header">
+          <h2>Bloc Chat</h2>
+          <h3> Available Rooms</h3>
+          <RoomList firebase= { firebase } />
+        </div>
       </div>
     );
   }
